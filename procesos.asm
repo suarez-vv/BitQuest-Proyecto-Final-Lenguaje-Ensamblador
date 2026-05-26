27340f4e44ea857434f4138be771ab4d9e9f4530
PaooZer <paola.dev.zo@gmail.com>
2026-05-26 13:51:32 -0600

    Función genérica para contar caracteres (monedas, llaves, etc)
    Implement 'contarCaracteres' function to count occurrences of a target character in a given map.

--- procesos.asm
+++ procesos.asm
@@ -2,3 +2,37 @@ bits 64
 default rel
 global casillas
 section .text
+
+; en c declaramos asi: int contarCaracteres(char *mapa, int totalCeldas, char objetivo)
+
+; rdi = direccion del mapa
+; rsi = total de celdas
+; rdx = caracter objetivo
+; eax = cantidad encontrada
+
+
+contarCaracteres:
+
+    xor eax, eax          ; contador = 0
+
+    cmp rsi, 0
+    jle .fin
+
+.loop:
+
+    mov r8b, [rdi]        ; leer caracter actual
+
+    cmp r8b, dl           ; comparar con objetivo
+    jne .noCoincide
+
+    inc eax               ; contador++
+
+.noCoincide:
+
+    inc rdi               ; siguiente celda
+    dec rsi               ; totalCeldas--
+
+    jnz .loop
+
+.fin:
+    ret
